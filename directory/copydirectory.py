d3572a349d23d21388a153545c0101c5d7266bfe
JHyun <jhyunahn226@gmail.com>
2022-03-05 16:22:50 +0900

    친절한 로그 추가

--- directory/copydirectory.py
+++ directory/copydirectory.py
@@ -9,19 +9,38 @@
 import os, sys, shutil
 
 def main():
+    for _ in range(100):
+        print("#", end='')
+    print('')
+
+    print(sys.argv[0] + " 시작...")
+    if len(sys.argv) != 3:
+        print("Error: Input 개수가 올바르지 않습니다.")
+        print(sys.argv[0] + " 실패:(")
+        return False
+
     origin = sys.argv[1]
     dest = sys.argv[2]
+
+    print(origin + " -> 이 폴더를")
+    print(dest + " -> 여기로 복사할게요.")
 
     files = os.listdir(origin)
 
     # dest 디렉토리가 없다면, 디렉토리 생성
     if not os.path.isdir(dest):
+        print("목적지 경로를 찾을 수 없네요ㅠㅠ 제가 생성할게요!")
         os.makedirs(dest)
 
     for file in files:
         if not os.path.exists(dest+'\\'+file):
             shutil.copy(origin+'\\'+file, dest+'\\'+file)
 
+    print(sys.argv[0] + " 성공:)")
+
+    for _ in range(100):
+        print("#", end='')
+    print('')
 
 if __name__ == "__main__":
     main()
--- directory/copyfile.py
+++ directory/copyfile.py
@@ -9,15 +9,33 @@
 import os, sys, shutil
 
 def main():
+    for _ in range(100):
+        print("#", end='')
+    print('')
+
+    print(sys.argv[0] + " 시작...")
+    if len(sys.argv) != 3:
+        print("Error: Input 개수가 올바르지 않습니다.")
+        print(sys.argv[0] + " 실패:(")
+        return False
+    
     origin = sys.argv[1]
     dest = sys.argv[2]
 
+    print(sys.argv[1] + " -> 이 파일을")
+    print(sys.argv[2] + " -> 여기로 복사할게요.")
+
     # dest 디렉토리가 없다면, 디렉토리 생성
     if not os.path.isdir(dest):
+        print("목적지 경로를 찾을 수 없네요ㅠㅠ 제가 생성할게요!")
         os.makedirs(dest)
 
     shutil.copy(origin, dest)
-
+    print(sys.argv[0] + " 성공:)")
+    
+    for _ in range(100):
+        print("#", end='')
+    print('')
 
 if __name__ == "__main__":
     main()
